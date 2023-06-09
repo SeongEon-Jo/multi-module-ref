3e32f500981e2d8254e17fda4831f0ea8248330e
SeongEon-Jo <meanaccto@naver.com>
2023-06-09 16:33:22 +0900

    initial commit

---
+++ core/persistence/src/test/java/com/example/persistence/PersistenceApplication.java
@@ -0,0 +1,11 @@
+package com.example.persistence;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class PersistenceApplication {
+
+	public static void main(String[] args) {
+	}
+
+}
